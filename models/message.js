2feb6fb57d1d3f3c76566e77d196e7ec392f86df
Tribhuwan <mathincosmos@gmail.com>
2023-04-26 17:39:40 +0530

    update package.json

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   text: { type: String, minLength: 1, maxLength: 100, required: true },
   user: { type: String, minLength: 1, maxLength: 30, required: true },
-  added: { type: Date, default: new Date() },
+  added: { type: Date, default: new Date().toLocaleDateString() },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
